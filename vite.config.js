d964370e43aadf6379f2c40bf572bcc776f4a68b
pavel-dev-bd <pavel.dev.bd@gmail.com>
2025-11-16 20:11:44 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import * as path from 'path';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
-
-  console.log('Loaded env:', env); // 🧠 Debugging output
-
   return {
     plugins: [react()],
     resolve: {
@@ -20,9 +17,5 @@ export default defineConfig(({ mode }) => {
       build: {
         outDir: 'dist',
       },
-<<<<<<< HEAD
-     
-=======
->>>>>>> 913186bdb32d44339277b90b71ecb9f21bf8441d
   };
 });
